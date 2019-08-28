05860430cadf67fc2ff7c522823e9757f56ece85
unknown <malloryjmathews@gmail.com>
2019-08-28 17:03:37 -0600

    part 5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -3,16 +3,16 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {image: 'https://assetsnffrgf-a.akamaihd.net/assets/m/502013285/univ/art/502013285_univ_lsr_xl.jpg'}
+  }
+  
+  
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src = {this.state.image} />
       </div>
     );
   }
